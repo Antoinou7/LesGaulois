3226cbed110ac54940409132de227d1f49362938
Antoine <antoine.deveze@univ-tlse3.fr>
2024-09-30 15:54:52 +0200

    Fin TP2
    Ajout du enum Equipement etc.

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,22 @@
+package personnages;
+
+public enum Equipement {
+    CASQUE("Casque"),
+    BOUCLIER("Bouclier");
+
+    private String nom;
+    // Constructeur pour initialiser l'attribut nom
+    private Equipement(String nom) {
+        this.nom = nom;
+    }
+
+    // Méthode pour récupérer le nom de l'équipement
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
-		assert force>0;
+		assert (force>0);
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
 	}
 
 	public String getNom() {
@@ -23,14 +27,50 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert (force>0);
+		int forceAvant = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert (force < forceAvant);
 		
 	}
+	
+	
+	public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+            case 2: // Le Romain a déjà deux équipements
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+
+            case 1: // Le Romain a un équipement, on vérifie s'il a déjà celui-là
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement.getNom() + " !");
+                } else {
+                    ajouterEquipement(equipement);
+                }
+                break;
+
+            case 0: // Le Romain n'a aucun équipement
+                ajouterEquipement(equipement);
+                break;
+        }
+    }
+	
+	
+	
+	private void ajouterEquipement(Equipement equipement) {
+        equipements[nbEquipement] = equipement;
+        nbEquipement++;
+        System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+    }
+	
+	
+	
+	
 	
 	@Override
 	public String toString() {
@@ -43,8 +83,10 @@ public class Romain {
 		Romain minus;
 		minus = new Romain("Minus", 6);
 		System.out.println(minus);
+		System.out.println(Equipement.CASQUE);
 		
 	}
+	
 	
 	
 }
